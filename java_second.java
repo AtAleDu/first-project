9b5152420b0c7995afc2f9f9d64b23819bacb651
Artur <arturdudarev60@gmail.com>
2023-02-17 09:31:22 +0500

    Мой не первый коммет!

---
+++ java_second.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+public class Course {
+
+	public static void main(String[] args) {
+		HashMap<String,String>statesCapitals = new HashMap<>();
+		statesCapitals.put("Russia", "Moscow");
+		statesCapitals.put("France", "Paris");
+		statesCapitals.put("Italia", "Rim");
+		
+
+	}	
+}

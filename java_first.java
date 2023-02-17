1d792a2b3440b6a9fd7b82ae86f9f3aefaac1fda
Artur <arturdudarev60@gmail.com>
2023-02-17 09:35:06 +0500

    Удалил вообще всё

--- java_first.java
+++ java_first.java
@@ -2,14 +2,6 @@ public class java_first {
 
 	public static void main(String[] args) {
 		HashMap<String,String>statesCapitals = new HashMap<>();
-		statesCapitals.put("Russia", "Moscow");
-		statesCapitals.put("France", "Paris");
-		statesCapitals.put("Italia", "Rim");
-		
-		System.out.println(statesCapitals.containsKey("Italia"));
-		System.out.println(statesCapitals.containsKey("Germany"));
-		
-		System.out.println(statesCapitals.containsValue("Paris"));
-		System.out.println(statesCapitals.values());
+
 	}	
 }
